365da3a445acc97d848786782caca791fa54a533
Gryffindor_Dodie <sultanovartur1873@gmail.com>
2022-08-31 18:55:17 +0300

    add publisher/subscriber pattern

--- L0/publisher_stan/publisher.go
+++ L0/publisher_stan/publisher.go
@@ -7,19 +7,20 @@ import (
 	"github.com/nats-io/stan.go"
 	"io/ioutil"
 	"log"
+	"time"
 )
 
 func main() {
-	modelList := []string{"../model", "../model1"}
+	modelList := []string{"../model.json", "../model1.json"}
 	sc, err := stan.Connect("test-cluster", "publisher")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer sc.Close()
-	//for i := 0; i < 100; i++ {
-	//	sc.Publish("foo", []byte("Здарова"))
-	//	time.Sleep(100 * time.Millisecond)
-	//}
+	for i := 0; i < 100; i++ {
+		sc.Publish("foo", []byte("Здарова"))
+		time.Sleep(100 * time.Millisecond)
+	}
 	mod := new(models.Model)
 	for _, s := range modelList {
 		file, err := ioutil.ReadFile(s)
