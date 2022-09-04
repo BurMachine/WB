8c14aa6368dcd4656dfb6609dc8f5237883bf810
Gryffindor_Dodie <sultanovartur1873@gmail.com>
2022-09-04 21:24:31 +0300

    second task

--- L1/L1.3/3.go
+++ L1/L1.3/3.go
@@ -3,37 +3,36 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 type arrStruct struct {
 	arr []int
+	res int
 	mu  sync.Mutex
 }
-
-//func pow(number int) {
-//	fmt.Printf("%d\n", number*number)
-//}
 
 func (s *arrStruct) pow(i int) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.arr[i] *= s.arr[i]
-	//fmt.Printf("%d\n", s.arr)
-	//s.mu.Unlock()
+	s.res += s.arr[i]
+	fmt.Println(s.res)
 }
 
 func main() {
 	arr := []int{2, 4, 6, 8, 10}
+	res := 0
+	//var wg sync.WaitGroup
 	var mu sync.Mutex
 	s := &arrStruct{
 		arr: arr,
 		mu:  mu,
+		res: res,
 	}
+	//wg.Add(len(s.arr))
 	for i := 0; i < len(s.arr); i++ {
 		go s.pow(i)
 	}
-	fmt.Println(s.arr)
-
-	time.Sleep(1 * time.Second)
+	//wg.Wait()
+	//fmt.Println(s.res)
 }
