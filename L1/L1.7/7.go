a73ed779ee7f557193d78eca83f51e79cd6f8450
Gryffindor_Dodie <sultanovartur1873@gmail.com>
2022-09-07 21:10:56 +0300

    add 7

--- L1/L1.7/7.go
+++ L1/L1.7/7.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"log"
+	"math/rand"
+	"sync"
+)
+
+type mapInfo struct {
+	*sync.RWMutex
+	ma map[int]int
+}
+
+func (m *mapInfo) Recording(key, value int, wg *sync.WaitGroup) {
+	m.Lock()
+	defer wg.Done()
+	defer m.Unlock()
+	m.ma[key] = value
+}
+
+func main() {
+	mapa := make(map[int]int)
+	wg := &sync.WaitGroup{}
+	m := &mapInfo{
+		RWMutex: &sync.RWMutex{},
+		ma:      mapa,
+	}
+	for i := 0; i < 100; i++ {
+		j := rand.Intn(100)
+		wg.Add(1)
+		go m.Recording(i, j, wg)
+	}
+	wg.Wait()
+	log.Println(m.ma)
+}
