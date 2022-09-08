d144df052b9b3d9d60107c3b9ecac87a007986fd
Gryffindor_Dodie <sultanovartur1873@gmail.com>
2022-09-08 16:48:52 +0300

    14 task add

--- L1/L1.13/13.go
+++ L1/L1.13/13.go
@@ -1,1 +1,11 @@
-package L1_13
+package main
+
+import "fmt"
+
+// просто синтаксическая особенность
+func main() {
+	a := 1
+	b := 2
+	b, a = a, b
+	fmt.Println(a, b)
+}
--- L1/L1.14/14.go
+++ L1/L1.14/14.go
@@ -1,1 +1,25 @@
-package L1_14
+package main
+
+import "fmt"
+
+func TypeDetermination(value interface{}) {
+	switch value.(type) {
+	case int:
+		fmt.Println("int type")
+	case string:
+		fmt.Println("string type")
+	case bool:
+		fmt.Println("bool type")
+	case chan interface{}:
+		fmt.Println("chan type")
+	}
+}
+
+func main() {
+	var value interface{}
+	value = true
+	//value = 12
+	//value = "qwerty"
+	//value := make(chan interface{})
+	TypeDetermination(value)
+}
