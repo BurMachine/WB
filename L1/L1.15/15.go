946b15de4dd19584ff01b3c3e97f3b4b2b3dc54c
Gryffindor_Dodie <sultanovartur1873@gmail.com>
2022-09-08 18:39:57 +0300

    add unfinished task 15

--- L1/L1.15/15.go
+++ L1/L1.15/15.go
@@ -1,1 +1,36 @@
-package L1_15
+package main
+
+import "fmt"
+
+var justString string
+
+func createHugeString(int) string {
+	var str string
+	fmt.Println(str)
+	// some code
+	return str
+}
+
+// У нас будет 924 неисполользуемых байта памяти, которые не сможет очистить сборщик мусора
+// Они будут висеть в столько, сколько работает наша программа
+// Поэтому лучше сразу выделять столько памяти, сколько нужно, или вместо среза копировать 100 элементов
+
+// TASK REALIZATION
+//func someFunc() {
+//	v := createHugeString(1 << 10)
+//	justString = v[:100]
+//}
+//
+//func main() {
+//	someFunc()
+//}
+
+// CORRECT VERSION
+
+func someFunc() {
+	justString = createHugeString(100)
+}
+
+func main() {
+	someFunc()
+}
