4300ac813ce104b96794fb8b2e2cdd2560c8a292
Gryffindor_Dodie <sultanovartur1873@gmail.com>
2022-10-10 18:35:43 +0300

    retrying builder v2 complete

--- L2/patterns/Builder/v2/main.go
+++ L2/patterns/Builder/v2/main.go
@@ -1,6 +1,8 @@
 package main
 
-//**************************
+import "fmt"
+
+//************************** ИНТЕРФЕЙС СТРОИТЕЛЯ
 
 type Builder interface {
 	SetHp()
@@ -13,13 +15,108 @@ func getAuto(name string) Builder {
 	if name == "supra" {
 		return newSupra()
 	} else if name == "challenger" {
-		return newChalle
+		return newChallenger()
 	}
+	return nil
 }
 
-//***************************
+//*************************** ОБЪЕКТ КОТОРЫЙ СТРОЯТ
 
 type Auto struct {
 	HorsePower     int
 	TwistingMoment int
+	Name           string
+}
+
+//*************************** Конкрутный строитель Toyota
+
+type Toyota struct {
+	HP          int
+	TwistMoment int
+	Name        string
+}
+
+func (t *Toyota) SetHp() {
+	t.HP = 399
+}
+
+func (t *Toyota) SetTwistingMoment() {
+	t.TwistMoment = 510
+}
+
+func (t *Toyota) GetAuto() Auto {
+	return Auto{
+		HorsePower:     t.HP,
+		TwistingMoment: t.TwistMoment,
+		Name:           t.Name,
+	}
+}
+
+func newSupra() *Toyota {
+	return &Toyota{Name: "Toyota Supra 80 DTM"}
+}
+
+//*************************** Конкрутный строитель Dodge
+
+type Dodge struct {
+	HP          int
+	TwistMoment int
+	Name        string
+}
+
+func (d *Dodge) SetHp() {
+	d.HP = 600
+}
+
+func (d *Dodge) SetTwistingMoment() {
+	d.TwistMoment = 800
+}
+
+func (d *Dodge) GetAuto() Auto {
+	return Auto{
+		HorsePower:     d.HP,
+		TwistingMoment: d.TwistMoment,
+		Name:           d.Name,
+	}
+}
+
+func newChallenger() *Dodge {
+	return &Dodge{Name: "Dodge Challenger Hellcat"}
+}
+
+//*************************** Общий сборщик Director
+
+type Director struct {
+	Builder Builder
+}
+
+func newDirector(b Builder) *Director {
+	return &Director{Builder: b}
+}
+
+func (d *Director) SetBuilderToDirector(b Builder) {
+	d.Builder = b
+}
+
+func (d *Director) BuildMachine() Auto {
+	d.Builder.SetHp()
+	d.Builder.SetTwistingMoment()
+	return d.Builder.GetAuto()
+}
+
+//*************************** Клиентский код
+
+func main() {
+	supra := getAuto("supra")
+	challenger := getAuto("challenger")
+
+	builder := newDirector(supra)
+	machine := builder.BuildMachine()
+
+	fmt.Println(machine.Name, machine.HorsePower, machine.TwistingMoment)
+
+	builder.SetBuilderToDirector(challenger)
+	machine = builder.BuildMachine()
+
+	fmt.Println(machine.Name, machine.HorsePower, machine.TwistingMoment)
 }
