b4291e656b1bf3ce92c247ac85f7d1cf99609b64
Gryffindor_Dodie <sultanovartur1873@gmail.com>
2022-10-10 18:00:54 +0300

    add v2

--- L2/patterns/Builder/v2/main.go
+++ L2/patterns/Builder/v2/main.go
@@ -1,1 +1,25 @@
-package v2
+package main
+
+//**************************
+
+type Builder interface {
+	SetHp()
+	SetTwistingMoment()
+
+	GetAuto() Auto
+}
+
+func getAuto(name string) Builder {
+	if name == "supra" {
+		return newSupra()
+	} else if name == "challenger" {
+		return newChalle
+	}
+}
+
+//***************************
+
+type Auto struct {
+	HorsePower     int
+	TwistingMoment int
+}
